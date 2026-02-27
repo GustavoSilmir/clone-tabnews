1a9256101404ce09a90a99f64dbfa6ce69e7aa86
Silmir <149710532+GustavoSilmir@users.noreply.github.com>
2026-02-27 05:35:23 +0000

    add  endpoint

---
+++ pages/api/v1/migrations/index.js
@@ -0,0 +1,34 @@
+import MigrationRunner from "node-pg-migrate";
+import { join } from "node:path";
+import database from "infra/database.js";
+
+export default async function migrations(request, response) {
+  const dbClient = await database.getNewClient();
+  const defaultMigrationsOptions = {
+    dbClient: dbClient,
+    dryRun: true,
+    dir: join("infra", "migrations"),
+    direction: "up",
+    verbose: true,
+    migrationsTable: "pgmigrations",
+  };
+  if (request.method === "GET") {
+    const pendingmigrations = await MigrationRunner(defaultMigrationsOptions);
+    await dbClient.end();
+    return response.status(200).json(pendingmigrations);
+  }
+
+  if (request.method === "POST") {
+    const migratedMigrations = await MigrationRunner({
+      ...defaultMigrationsOptions,
+      dryRun: false,
+    });
+    await dbClient.end();
+    if (migratedMigrations.length > 0) {
+      return response.status(201).json(migratedMigrations);
+    }
+    return response.status(200).json(migratedMigrations);
+  }
+
+  return response.status(405).end();
+}
